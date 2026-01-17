bfe2a3327e9ee7ad1272d696e07bcfff0079720c
viswanath234 <viswabai859@gmail.com>
2026-01-17 22:19:38 +0530

    third commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api/v1`;
 
 // Fetch all products
 export const fetchProducts = async (params = {}) => {
